6d2d6ca8b2834a9a008e83a080b038a5e9a2461c
MENG <mzrwalzy@163.com>
2022-06-28 15:11:46 +0800

    fix: alter root_path to crpc and annotate DEFAULT_ZOOKEEPER_ADDRESS

--- crpc-spring-boot-starter/src/main/java/github/charon/registry/zk/util/CuratorUtils.java
+++ crpc-spring-boot-starter/src/main/java/github/charon/registry/zk/util/CuratorUtils.java
@@ -1,7 +1,5 @@
 package github.charon.registry.zk.util;
 
-import github.charon.common.enums.RpcConfig;
-import github.charon.common.utils.PropertiesFileUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
@@ -15,7 +13,6 @@ import org.apache.zookeeper.CreateMode;
 import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -24,11 +21,11 @@ import java.util.concurrent.TimeUnit;
 public class CuratorUtils {
     private static final int BASE_SLEEP_TIME = 1000;
     private static final int MAX_RETRIES = 3;
-    public static final String ZK_REGISTER_ROOT_PATH = "/my-rpc";
+    public static final String ZK_REGISTER_ROOT_PATH = "/crpc";
     private static final Map<String, List<String>> SERVICE_ADDRESS_MAP = new ConcurrentHashMap<>();
     private static final Set<String> REGISTERED_PATH_SET = ConcurrentHashMap.newKeySet();
     private static CuratorFramework zkClient;
-    private static final String DEFAULT_ZOOKEEPER_ADDRESS = "127.0.0.1:2181";
+//    private static final String DEFAULT_ZOOKEEPER_ADDRESS = "127.0.0.1:2181";
 
     private CuratorUtils() {
     }
